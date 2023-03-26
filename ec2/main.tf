17714cf8ca24eccdc8cecde49b084accc46742f0
Arpita Patnaik <32553590+arpita497@users.noreply.github.com>
2023-03-26 21:54:28 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,1 +1,18 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.3.0"
 
+  name = "ltiinstance"
+
+  ami                    = "ami-08f0bc76ca5236b20"
+  instance_type          = "t2.micro"
+  key_name               = "sydney-keypair"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-09d0c7268bedc251a"]
+  subnet_id              = "subnet-0d6190df283450c4e"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
